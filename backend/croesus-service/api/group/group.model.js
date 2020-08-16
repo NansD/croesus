68000bb6a62fabcbb29bbb251e277ddb82611f80
Nans Dumortier <dumortier.nans@gmail.com>
2020-08-16 12:20:25 +0200

    feat(groups): add participants

--- backend/croesus-service/api/group/group.model.js
+++ backend/croesus-service/api/group/group.model.js
@@ -1,10 +1,24 @@
 const { Schema } = require('mongoose');
 const ExpenseModel = require('../expense/expense.model');
 
+const ParticipantSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  customRate: {
+    type: Number,
+  },
+});
+
 module.exports = new Schema({
   name: {
     type: String,
     required: true,
+  },
+  participants: {
+    type: [ParticipantSchema],
+    default: [],
   },
   expenses: {
     type: [ExpenseModel],
